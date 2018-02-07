1abf7529de3786ed85e58a5c58d9c6ea3141465d
kelthuzad <kelthuzad@localhost.localdomain>
2018-02-06 22:04:22 -0500

    Actualizacion y creacion de nuevos comandos,
    Se a modificado el origen de las cadenas de texto predefinidas a un
    archivo externo, text.py, asi mismo se han agregado las funciones
    faltantes que se esperaba.
    
    Ahun se tienen problemas con el mensaje disparado por el evento de nuevo
    miembro.

--- nagini.py
+++ nagini.py
@@ -6,15 +6,13 @@ import telebot # Librería de la API del bot.
 from telebot import types # Tipos para la API del bot.
 import time
 
+import text
+
 bot = telebot.TeleBot(token)
 
-ayuda = """Puedes utilizar los siguientes comandos : 
-            /ayuda  -   Guia para utilizar el bot. 
-            /info   -   Informacion de interes sobre el grupo.
-            /rules  -   Reglas del grupo. 
-            /hola   -   Saludo del Bot"""
 
-#Listener
+
+#Listener [Se ejecuta cada ves que el bot recibe algun mensaje]
 def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
     for m in messages: # Por cada dato 'm' en el dato 'messages'
         cid = m.chat.id # El Cid es el identificador del chat los negativos son grupos y positivos los usuarios
@@ -27,12 +25,53 @@ def listener(messages): # Con esto, estamos definiendo una función llamada 'lis
         f.close() # Cerramos el fichero para que se guarde.
         print(mensaje) # Imprimimos el mensaje en la terminal, que nunca viene mal :) 
 
+
+#Mensaje de bienvenida para nuevos usuarios en un chat grupal
+@bot.message_handler(content_type=["new_chat_members"])
+def bienvenida(m):
+    bot.send_message(m.chat.id,text.bienvenida)
+
+#Mensaje de despedida para usuarios en un chat grupal
+@bot.message_handler(content_type=["left_chat_member"])
+def despedida(m):
+    bot.send_message(m.chat.id,text.adios)
+
+
+#Mensaje de ayuda
 @bot.message_handler(commands=['ayuda']) # Indicamos que lo siguiente va a controlar el comando '/ayuda'
 def command_ayuda(m): # Definimos una función que resuleva lo que necesitemos.
     cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
     bot.send_chat_action(cid, 'typing') # Enviando ...
     time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
-    bot.send_message( cid, ayuda) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+    bot.send_message( cid, text.ayuda) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+#Mensaje de info
+@bot.message_handler(commands=['info']) # Indicamos que lo siguiente va a controlar el comando '/ayuda'
+def command_info(m): # Definimos una función que resuleva lo que necesitemos.
+    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+    bot.send_chat_action(cid, 'typing') # Enviando ...
+    time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
+    bot.send_message( cid, text.info) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+#Mensaje con las reglas del grupo
+@bot.message_handler(commands=['rules'])
+def command_rules(m):
+    cid = m.chat.id
+    rules = []
+    for a in range(5):
+        if a > 0:
+            r = open('./img/rules/'+str(a)+'.jpeg','rb')
+            rules.append(r)
+
+    bot.send_chat_action(cid,'typing')
+    time.sleep(1)
+    bot.send_message(cid, text.rules)
+    for a in range(4):
+        bot.send_photo(cid,rules[a])
+
+@bot.message_handler(commands=['hola'])
+def command_hola(m):
+    bot.send_message(m.chat.id,text.bienvenida)
 
 # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
 bot.set_update_listener(listener) 
---
+++ text.py
@@ -0,0 +1,18 @@
+ayuda = """Puedes utilizar los siguientes comandos : 
+/ayuda  -   Guia para utilizar el bot. 
+/info   -   Informacion de interes sobre el grupo.
+/rules  -   Reglas del grupo. 
+/hola   -   Saludo del Bot"""
+
+bienvenida = """Hola!!
+Bienvenido al chat de programacion en python.
+dirigido por la Asociacion Peruana de Programacion en Python. (APPPY)"""
+
+rules = """Las reglas estan descritas en las siguientes imagenes."""
+
+info = """Este es un grupo creado por la 
+Asociacion Peruana de Programacion en Pythno (APPPy)
+En este grupo se promueve el apoyo mutuo y el debate
+sobre los temas referidos en el titulo del mismo."""
+
+adios = """Adios, gracias por participar de nuestro coloquio."""
